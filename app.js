74d5f3c6138855dfd6b205cda556a2f63baaf768
Alan Silverio <alan.jse@outlook.com>
2026-05-24 02:27:44 -0600

    Se agregaron cambios en carrusel y animacion de hojas

--- app.js
+++ app.js
@@ -6,6 +6,170 @@ const weddingDate = new Date(
     'Sep 27, 2027 00:00:00'
 ).getTime();
 
+/* =========================================
+   GENERADOR DE HOJAS CALLENDO
+========================================= */
+
+const leavesContainer =
+    document.querySelector('.leaves');
+
+/* =========================================
+   CREATE LEAVES
+========================================= */
+
+for (let i = 0; i < 35; i++) {
+
+    const leaf =
+        document.createElement('span');
+
+    leaf.classList.add('leaf');
+
+
+    /* RANDOM SIZE */
+
+    const size =
+        Math.random() * 18 + 10;
+
+    leaf.style.width =
+        `${size}px`;
+
+    leaf.style.height =
+        `${size}px`;
+
+
+    /* RANDOM POSITION */
+
+    leaf.style.left =
+        `${Math.random() * 100}%`;
+
+
+    /* RANDOM COLORS */
+
+    const colors = [
+
+        'linear-gradient(135deg,#d4af6a,#b8924f)',
+
+        'linear-gradient(135deg,#e6c27a,#c49b57)',
+
+        'linear-gradient(135deg,#f0d89c,#d4af6a)'
+
+    ];
+
+    leaf.style.background =
+        colors[
+        Math.floor(
+            Math.random() * colors.length
+        )
+        ];
+
+
+    /* RANDOM SPEED */
+
+    leaf.style.animationDuration =
+        `${Math.random() * 8 + 8}s`;
+
+
+    /* RANDOM DELAY */
+
+    leaf.style.animationDelay =
+        `${Math.random() * 10}s`;
+
+
+    /* RANDOM ROTATION */
+
+    leaf.style.transform =
+        `rotate(${Math.random() * 360}deg)`;
+
+
+    leavesContainer.appendChild(leaf);
+
+}
+
+/* =========================
+   PREMIUM CAROUSEL
+========================= */
+
+const slides = document.querySelectorAll('.carousel-slide');
+const carousel =
+    document.querySelector('.gallery-carousel');
+let current = 0;
+let startX = 0;
+
+/* =========================
+   SHOW SLIDE
+========================= */
+const show = (index) => {
+
+    slides.forEach((slide, i) => {
+
+        slide.classList.toggle(
+            'active',
+            i === index
+        );
+
+    });
+
+};
+
+/* =========================
+   MOVE SLIDE
+========================= */
+
+const move = (step) => {
+    current =
+        (current + step + slides.length)
+        % slides.length;
+
+    show(current);
+};
+
+/* =========================
+   AUTOPLAY
+========================= */
+
+setInterval(() => move(1), 4000);
+
+/* =========================
+   TOUCH EVENTS (MOBILE)
+========================= */
+
+carousel.addEventListener('touchstart', (e) => {
+    startX = e.touches[0].clientX;
+});
+
+
+carousel.addEventListener('touchend', (e) => {
+    const diff =
+        startX - e.changedTouches[0].clientX;
+    if (Math.abs(diff) > 50) {
+        move(diff > 0 ? 1 : -1);
+    }
+});
+
+
+/* =========================
+   MOUSE EVENTS (DESKTOP)
+========================= */
+
+carousel.addEventListener('mousedown', (e) => {
+
+    startX = e.clientX;
+
+});
+
+
+carousel.addEventListener('mouseup', (e) => {
+
+    const diff =
+        startX - e.clientX;
+
+    if (Math.abs(diff) > 50) {
+
+        move(diff > 0 ? 1 : -1);
+
+    }
+
+});
 
 /* ========================= */
 /* COUNTDOWN FUNCTION */
